d19b5d5aa139d5b4d92167b8820567431fcaa0c7
it'z abir <abiralmahmud42@gmail.com>
2024-05-20 18:04:44 +0600

    9th commit

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,32 +1,44 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
- 
+  const location = useLocation();
+  const [activePath, setActivePath] = useState(location.pathname);
+
+  const handleLinkClick = (path) => {
+    setActivePath(path);
+  };
+
   return (
-     <>
-     <div className='navbar'>
-     <div className='box'>
+    <>
+      <div className='navbar'>
+        <div className='box'>
+        </div>
+        <nav>
+          <div>
+            <div className='first'>
+              <div className="logo"></div>
+              <p className="goals">Goals</p>
+            </div>
+            <ul className='hhh'>
+              <Link to="/" className={`hhh ${activePath === '/' ? 'active' : ''}`} onClick={() => handleLinkClick('/')}>
+                <li><i className="fas fa-home"></i> Home</li>
+              </Link>
+              <Link to="/favorites" className={`hhh ${activePath === '/favorites' ? 'active' : ''}`} onClick={() => handleLinkClick('/favorites')}>
+                <li><i className="fas fa-heart"></i> Favorites</li>
+              </Link>
+              <Link to="/inProgress" className={`hhh ${activePath === '/inProgress' ? 'active' : ''}`} onClick={() => handleLinkClick('/inProgress')}>
+                <li><i className="fas fa-tasks"></i> In Progress</li>
+              </Link>
+              <Link to="/complete" className={`hhh ${activePath === '/complete' ? 'active' : ''}`} onClick={() => handleLinkClick('/complete')}>
+                <li><i className="fas fa-check-circle"></i> Complete</li>
+              </Link>
+            </ul>
+          </div>
+        </nav>
       </div>
-      <nav>
-      <div>
-        <div className='first'>
-            <div className="logo"></div>
-            <p className="goals">Goals</p>
-        </div>     
-        <ul className='hhh'>
-          <Link to="/" className='hhh'><li> <i className="fas fa-home"></i> Home </li></Link>
-          <Link to="/favorites"className='hhh'><li> <i className="fas fa-heart"></i> Favorites </li></Link>
-          <Link to="/inProgress" className='hhh'><li> <i className="fas fa-tasks"></i> In Progress </li></Link>
-          <Link to="/complete" className='hhh'><li> <i className="fas fa-check-circle"></i> Complete </li></Link>
-        </ul>
-      </div>
-      </nav>
-     </div>
-     
     </>
-    
   );
 }
 

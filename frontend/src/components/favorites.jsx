79baa317ef584b00b813db64eab81dfff152f5bc
it'z abir <abiralmahmud42@gmail.com>
2024-05-20 18:30:42 +0600

    10th commit

---
+++ frontend/src/components/favorites.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+
+const favorites = () => {
+  return (
+    <>
+      <div className="container">
+     </div>
+     <div className="favorite-goals">
+      <h1>Favorite Goals</h1>
+      <p>(0)</p>
+    </div>
+      
+    </>
+  )
+}
+
+export default favorites

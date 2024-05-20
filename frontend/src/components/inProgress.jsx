3af9fa20230fd6c83820671ac3f5dbb7843639d4
it'z abir <abiralmahmud42@gmail.com>
2024-05-20 13:42:06 +0600

    commiting inProgress

--- frontend/src/components/inProgress.jsx
+++ frontend/src/components/inProgress.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 
 const inProgress = () => {
@@ -9,7 +7,6 @@ const inProgress = () => {
     <>
      <div className='home'>
       <div className="container">
-      {/* <button className="center-button">  <FontAwesomeIcon icon={faPlus} />  Click Me</button> */}
      </div>
      <div className="favorite-goals">
       <h1>In-Progress Goals</h1>
